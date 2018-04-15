5eb37a59df4ef57b263e29f1962ec4730c4a5b6d
benzsuankularb <benzsk130@gmail.com>
2018-04-16 00:32:33 +0700

    commit

---
+++ Weavers/ModuleWeaver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+using System.Collections.Generic;
+using Mono.Cecil.Rocks;
+using Fody;
+
+public partial class ModuleWeaver : BaseModuleWeaver
+{
+	
+	public override void Execute()
+	{
+		TESTONLY();
+	}
+
+	void TESTONLY () {
+		ModuleDefinition.ImportReference(typeof(Action)).Resolve();
+		ModuleDefinition.ImportReference(typeof(Action)).Resolve().GetConstructors();
+		ModuleDefinition.ImportReference(typeof(Action)).Resolve().GetConstructors().First();
+	}
+
+	public override IEnumerable<string> GetAssembliesForScanning()
+	{
+		return Array.Empty<string>();
+	}
+}
